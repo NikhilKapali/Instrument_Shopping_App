d1036872361b3e2b26d615e53208fde6ccb6b49c
NikhilKapali <nikhilkapali12@gmail.com>
2021-02-08 15:55:12 +0545

    Up till Confirm Order page

--- lib/orderpage/ConfirmOrderPage.dart
+++ lib/orderpage/ConfirmOrderPage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_app/StylesScheme.dart';
+import 'TrackOrderPage.dart';
 
 class ConfirmOrderPage extends StatelessWidget {
   @override
@@ -39,6 +41,145 @@ class _confirmOrderPageState extends State<confirmOrderPage> {
           )
         ],
       ),
+      body: Container(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 200,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                image: AssetImage("assets/images/onBoard2.png"),
+              )),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Text(
+              "Arigatauu for choosing Warera",
+              style: headingStyle,
+            ),
+            Text(
+              "Your order has been placed and Deliver Your weapon ON TIME!!",
+              style: contentStyle,
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Order ID",
+                  style: headingStyle,
+                ),
+                Container(
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    gradient: gradientStyle,
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                  ),
+                  child: Text(
+                    "1001",
+                    style: headingStyle.copyWith(color: Colors.white),
+                  ),
+                )
+              ],
+            ),
+            divider(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Weapon Count",
+                  style: headingStyle,
+                ),
+                Text(
+                  "5",
+                  style: headingStyle.copyWith(color: Colors.white),
+                ),
+              ],
+            ),
+            divider(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Final Amount",
+                  style: headingStyle,
+                ),
+                Text(
+                  "\$1400",
+                  style: headingStyle.copyWith(color: Colors.white),
+                ),
+              ],
+            ),
+            divider(),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Pick up Date & time",
+                  style: headingStyle,
+                ),
+                Text(
+                  "Wed, 08 Feb, 2021. 10:00 AM to 12:00 PM",
+                  style:
+                      contentStyle.copyWith(fontSize: 16, color: Colors.grey),
+                ),
+              ],
+            ),
+            divider(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Payment Method",
+                  style: headingStyle,
+                ),
+                Text(
+                  "Cash",
+                  style: headingStyle.copyWith(color: Colors.white),
+                ),
+              ],
+            ),
+            Expanded(child: Container()),
+            InkWell(
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => TrackOrderPage()));
+              },
+              child: Container(
+                padding: EdgeInsets.all(20),
+                height: 70,
+                decoration: BoxDecoration(
+                  gradient: gradientStyle,
+                ),
+                child: Center(
+                  child: Text(
+                    "TRACK ORDER BITCHH??",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Container divider() {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 15),
+      height: 1,
+      color: Colors.grey,
     );
   }
 }
---
+++ lib/orderpage/TrackOrderPage.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class TrackOrderPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: trackOrderPage(),
+    );
+  }
+}
+
+class trackOrderPage extends StatefulWidget {
+  @override
+  _trackOrderPageState createState() => _trackOrderPageState();
+}
+
+class _trackOrderPageState extends State<trackOrderPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
